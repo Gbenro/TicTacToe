8aa2b7ec7b088b9e10c129bc6c5ed4c4b67143ec
gbenro <gbenro_adesoye@yahoo.com>
2024-02-04 17:55:24 -0600

    updated state for game play log

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-import  Player from "./components/Player"
+import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import { useState } from "react"
+import Log from "./components/Log";
 function App() {
-  const [activePlayer, setActivePlayer]=useState('X');
-  function handleSelectSquare(){
-    setActivePlayer((currActivePlayer)=>currActivePlayer === 'X'? 'O' : "X")
+  const [gameTurns, setGameTurns] = useState([])
+  const [activePlayer, setActivePlayer] = useState('X');
+  function handleSelectSquare() {
+    setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : "X");
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X"
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = 'O'
+      }
+
+      const updatedTurns = [
+        {
+          square: { row: rowIndex, col: colIndex },
+          player: currentPlayer
+        }, ...prevTurns
+      ]
+      return updatedTurns;
+    });
   }
 
   return (
-   <main>
-    <div id = "game-container">
-      <ol id = "players" className="highlight-player">
-       <Player initialName = "player 1" symbol = "X" isActive={activePlayer === "X"}/>
-       <Player initialName= "player 2" symbol = "O"  isActive={activePlayer === "O"}/>
-       
-      </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName="player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player initialName="player 2" symbol="O" isActive={activePlayer === "O"} />
+
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
 
 
-    </div>
-    LOG
-   </main>
+      </div>
+      <Log />
+    </main>
   )
 }
 
