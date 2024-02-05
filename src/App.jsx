42f91eb31e1c941a9e94f4afa0e7122b12cc8209
gbenro <gbenro_adesoye@yahoo.com>
2024-02-04 20:55:33 -0600

    show player that won

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,13 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([])
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2"
+  })
   let activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array =>[...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -46,14 +50,21 @@ function App() {
     if (
       firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner= firstSquareSymbol;
+      winner= players[firstSquareSymbol];
 
     }
   }
 
   const hasdraw = gameTurns.length === 9 && !winner;
 
-
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers =>{
+      return {
+        ...prevPlayers,
+        [symbol]:newName
+      }
+    })
+  }
   function handleSelectSquare(rowIndex, colIndex) {
 
     setGameTurns((prevTurns) => {
@@ -73,15 +84,19 @@ function App() {
     });
   }
 
+  function handleRestart(){
+    setGameTurns([])
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="player 1" symbol="X" isActive={activePlayer === "X"} />
-          <Player initialName="player 2" symbol="O" isActive={activePlayer === "O"} />
+          <Player initialName="player 1" symbol="X" isActive={activePlayer === "X"} onChangeName= {handlePlayerNameChange} />
+          <Player initialName="player 2" symbol="O" isActive={activePlayer === "O"}  onChangeName= {handlePlayerNameChange}/>
 
         </ol>
-        {(winner || hasdraw) && <GameOver  winner ={winner}/>}
+        {(winner || hasdraw) && <GameOver  winner ={winner} onRestart={handleRestart}/>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
 
 
