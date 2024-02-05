f832cf37b2c590a65e587202cdb10be6f5b29b22
gbenro <gbenro_adesoye@yahoo.com>
2024-02-04 19:01:18 -0600

    added disabled to buttons when clicked

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -32,7 +32,7 @@ for (const turn of turns){
             {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}> <ol>
                     {row.map((playerSymbol, colIndex) => (<li key={colIndex}>
-                        <button onClick={()=>onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                        <button disabled={playerSymbol !== null} onClick={()=>onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
                     </li>))}
                 </ol> </li>
             ))}
